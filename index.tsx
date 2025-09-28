9b7c52422da41f0be4489ac0424453382f8b3a05
0xward <aradeawardana97@gmail.com>
2025-09-29 04:30:46 +0700

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import WalletConnectButton from "./components/WalletConnectButton";
+
+function App() {
+  return (
+    <div>
+      <h1>Minswap Wallet Connect Integration</h1>
+      <WalletConnectButton />
+    </div>
+  );
+}
